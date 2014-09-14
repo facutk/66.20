bfb7b55847ff860e8703bd4ba1c945b697802bd3
facu.tk <facu.tk@gmail.com>
2014-09-14 19:20:20 -0300

    parse_resolution tests

--- tp0/src/parse_opt.c
+++ tp0/src/parse_opt.c
@@ -22,3 +22,32 @@ int parse_width( char * param, double * result ) {
     fprintf( stderr, "fatal: invalid width specification.\n" );
     return 1;
 }
+
+int parse_height( char * param, double * result ) {
+    double height;
+    int scanned = sscanf( param, "%lf", &height );
+    if ( scanned == 1 ) {
+        if ( height > 0 ) {
+            *result = height;
+            return 0;
+        }
+    }
+    fprintf( stderr, "fatal: invalid height specification.\n" );
+    return 1;
+}
+
+int parse_resolution( char * param, double * res_x, double * res_y ) {
+    double _res_x;
+    double _res_y;
+
+    int scanned = sscanf( param, "%lfx%lf", &_res_x, &_res_y );
+    if ( scanned == 2 ) {
+        if ( ( _res_x > 0) && (_res_y > 0) ) {
+            *res_x = _res_x; 
+            *res_y = _res_y; 
+            return 0;
+        }
+    }
+    fprintf( stderr, "fatal: invalid resolution specification.\n" );
+    return 1;
+}
--- tp0/test/parse_opt_test.c
+++ tp0/test/parse_opt_test.c
@@ -1,6 +1,9 @@
 #include "CuTest.h"
 #include <stdio.h>
 
+/* 
+ * parse_width() tests
+ * */
 void test_parse_width_gets_1_returns_1(CuTest *tc) {
     double expected = 1;
     double actual;
@@ -56,13 +59,123 @@ void test_parse_width_gets_negative_halts(CuTest *tc) {
     CuAssertIntEquals(tc, expected, actual);
 }
 
+/* 
+ * parse_height() tests
+ * */
+void test_parse_height_gets_1_returns_1(CuTest *tc) {
+    double expected = 1;
+    double actual;
+    double delta = 0.00001;
+    char param[] = "1";
+
+    int result = parse_height( param, &actual );
+
+    CuAssertDblEquals(tc, expected, actual, delta);
+}
+
+void test_parse_height_gets_10_returns_10(CuTest *tc) {
+    double expected = 10;
+    double actual;
+    double delta = 0.00001;
+    char param[] = "10";
+
+    int result = parse_height( param, &actual );
+
+    CuAssertDblEquals(tc, expected, actual, delta);
+}
+
+void test_parse_height_gets_A_halts(CuTest *tc) {
+
+    char param[] = "A";
+    double returned;
+
+    int expected = 1;
+    int actual = parse_height( param, &returned );
+
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+void test_parse_height_gets_empty_halts(CuTest *tc) {
+
+    char param[] = "";
+    double returned;
+
+    int expected = 1;
+    int actual = parse_height( param, &returned );
+
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+void test_parse_height_gets_negative_halts(CuTest *tc) {
+
+    char param[] = "-1";
+    double returned;
+
+    int expected = 1;
+    int actual = parse_height( param, &returned );
+
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+/* 
+ * parse_resolution() tests
+ * */
+void test_parse_resolution_gets_empty_halts(CuTest *tc) {
+    int expected = 1;
+    double res_x;
+    double res_y;
+    char param[] = "";
+
+    int actual = parse_resolution( param, &res_x, &res_y );
+
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+void test_parse_resolution_gets_16x12_returns_16x12(CuTest *tc) {
+    double expected_x = 16;
+    double expected_y = 12;
+    double delta = 0.00001;
+    double actual_x;
+    double actual_y;
+    char param[] = "16x12";
+
+    int actual = parse_resolution( param, &actual_x, &actual_y );
+
+    CuAssertDblEquals(tc, expected_x, actual_x, delta);
+    CuAssertDblEquals(tc, expected_y, actual_y, delta);
+}
+
+void test_parse_resolution_gets_negative_halts(CuTest *tc) {
+    double res_x;
+    double res_y;
+    char param[] = "-16x12";
+
+    int expected = 1;
+    int actual = parse_resolution( param, &res_x, &res_y );
+
+    CuAssertIntEquals(tc, expected, actual);
+}
+
 CuSuite* parse_opt_tests_get_suite() {
     CuSuite* suite = CuSuiteNew();
+
+    // parse_width()
     SUITE_ADD_TEST(suite, test_parse_width_gets_1_returns_1);
     SUITE_ADD_TEST(suite, test_parse_width_gets_10_returns_10);
     SUITE_ADD_TEST(suite, test_parse_width_gets_A_halts );
     SUITE_ADD_TEST(suite, test_parse_width_gets_empty_halts );
     SUITE_ADD_TEST(suite, test_parse_width_gets_negative_halts );
 
+    // parse_height()
+    SUITE_ADD_TEST(suite, test_parse_height_gets_1_returns_1);
+    SUITE_ADD_TEST(suite, test_parse_height_gets_10_returns_10);
+    SUITE_ADD_TEST(suite, test_parse_height_gets_A_halts );
+    SUITE_ADD_TEST(suite, test_parse_height_gets_empty_halts );
+    SUITE_ADD_TEST(suite, test_parse_height_gets_negative_halts );
+
+    // parse_resolution()
+    SUITE_ADD_TEST(suite, test_parse_resolution_gets_empty_halts );
+    SUITE_ADD_TEST(suite, test_parse_resolution_gets_16x12_returns_16x12 );
+    SUITE_ADD_TEST(suite, test_parse_resolution_gets_negative_halts );
     return suite;
 }
