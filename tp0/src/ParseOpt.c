9157f0dab155d0d50040efcfbedaba5850ebe1db
facu.tk <facu.tk@gmail.com>
2014-09-14 15:47:23 -0300

    Initial testable version

---
+++ tp0/src/ParseOpt.c
@@ -0,0 +1,14 @@
+/*
+ ==========================================================================
+ Name        : ParseOpt.c
+ Author      : Tkaczyszyn, Facundo
+ Version     : 1.0
+ Description : Utility module to handle command line user interface
+ ==========================================================================
+ */
+
+#include <stdio.h>
+
+int genOne() {
+    return 0;
+}
---
+++ tp0/src/main.c
@@ -0,0 +1,20 @@
+/*
+===========================================================================
+ Name        : main.c
+ Author      : Tkaczyszyn, Facundo
+ Version     : 1.0
+ Description : 66.20 TP0 - Mandelbrot, Version 1.0
+===========================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    int ans;
+    ans = genOne();
+    printf("%d\n", ans);
+    return 0;
+
+}
+
