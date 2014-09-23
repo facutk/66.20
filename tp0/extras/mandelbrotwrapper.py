40f9d67ad59d7e5ca3aef5772821dfe960226d13
facu.tk <facu.tk@gmail.com>
2014-09-23 01:29:20 -0300

    mandelbrotwapper remove fileaccess

--- tp0/extras/mandelbrotwrapper.py
+++ tp0/extras/mandelbrotwrapper.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python
-import os, pgm2pil, subprocess
+import os, subprocess
+import StringIO
 from flask import Flask, request, render_template, send_file
 from PIL import Image
+import numpy
+
+def pgmdata2pil(pgmdata):
+    header = None
+    width = None
+    height = None
+    maxGray = None
+    data = []
+    pgmlist = pgmdata.splitlines()
+
+    for line in pgmlist:
+        stripped = line.strip()
+
+        if stripped[0] == '#': 
+            continue
+        elif header == None: 
+            if stripped != 'P2': return None
+            header = stripped
+        elif width == None:
+            width = int( stripped )
+        elif height == None:
+            height = int( stripped )
+        elif maxGray == None:
+            maxGray = int(stripped)
+        else:
+            for item in stripped.split():
+                data.append(int(item.strip()))
+
+    return numpy.reshape(data, ( height, width ) )/float(maxGray)*255
+
+def imagefrompgmdata( pgmdata ):
+
+    pgm = pgmdata2pil(pgmdata)
+    if pgm is not None:
+
+        return Image.fromarray(pgm)
+
+def giffrompgm( pgmdata ):
+
+    im = imagefrompgmdata( pgmdata )
+    output = StringIO.StringIO()
+    im = im.convert("RGB")
+    im.save(output, format='GIF')
+    output.seek(0)
+
+    return output
+
 
 app = Flask(__name__)
 
@@ -19,9 +67,8 @@ def mandelbrot():
     center = request.args.get('center','')
     w = request.args.get('w','')
     h = request.args.get('h','')
-    fname = path + "/generated.pgm"
 
-    #create image
+    #call binary
     call_string = []
     call_string.append( binary )
     if res:
@@ -32,18 +79,14 @@ def mandelbrot():
         call_string.append( "-w%s"%( w ) )
     if h:
         call_string.append( "-H%s"%( h ) )
-    call_string.append( "-o%s"%( fname ) )
+    call_string.append( "-o-" )
+    pgmdata = subprocess.check_output( call_string )
 
-    subprocess.call( call_string )
-
-    #convert image
-    gif_name = fname + ".gif"
-    im = Image.open( fname )
-    im = im.convert("RGB")
-    im.save( gif_name )
+    #convert to gif
+    gifdata = giffrompgm( pgmdata )
 
     #send image
-    return send_file( gif_name, mimetype='image/gif' )
+    return send_file( gifdata, mimetype='image/gif' )
 
 if __name__ == '__main__':
     app.run(debug = True)
